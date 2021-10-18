738b613e3d98e9312d2d6ae2949a09a8e693cd8a
Jonas Brusokas <jbrusokas@gmail.com>
2021-10-18 13:14:18 +0200

    Some bs pickling problem

--- forecasting/dataset.py
+++ forecasting/dataset.py
@@ -1,31 +1,13 @@
+from typing import Optional
+
 import numpy as np
 import pandas as pd
 
 import torch
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 
-class DataModule:
-
-    def __init__(self,
-                 df: pd.DataFrame,
-                 memory: int,
-                 horizon: int,
-                 batch_size: int,
-                 flatten_xs: bool,
-                 ):
-        pass
-
-    def setup(self):
-        pass
-
-    def train_dataloader(self):
-        pass
-
-    def val_dataloader(self):
-        pass
-
-    def test_dataloader(self):
-        pass
+from sklearn.preprocessing import StandardScaler
+from sktime.forecasting.model_selection import temporal_train_test_split
 
 class TSDataset(Dataset):
     '''
@@ -37,12 +19,12 @@ class TSDataset(Dataset):
     Suitable as an input to RNNs.
     '''
     def __init__(self,
-                 ts: np.ndarray,
+                 ts_tensor: torch.tensor,
                  memory: int,
                  horizon: int,
                  flatten: bool = False,
                  ):
-        self.ts_tensor = torch.tensor(ts).float()
+        self.ts_tensor = ts_tensor
         self.memory_length = memory
         self.horizon_length = horizon
         self.flatten = flatten
@@ -52,7 +34,6 @@ class TSDataset(Dataset):
         return self.ts_tensor.__len__() - self.memory_length - self.horizon_length + 1
 
     def __getitem__(self, index):
-        # return (self.X[index:index+self.seq_len], self.y[index+self.seq_len-1])
         x_border = index + self.memory_length
         y_border = x_border + self.horizon_length
 
@@ -61,15 +42,66 @@ class TSDataset(Dataset):
 
         return (x,y) if self.flatten else (x.unsqueeze(1), y.unsqueeze(1))
 
+class DataModule:
+
+    def __init__(self,
+                 df: pd.DataFrame,
+                 memory: int,
+                 horizon: int,
+                 batch_size: int,
+                 flatten_xs: bool,
+                 error_bound: Optional[int],
+                 ):
+        self.df = df
+
+        if (error_bound is None):
+            column = 'value-R'
+        else:
+            column = f'value-E{error_bound}'
+
+        self.series = self.df[column]
+        self.memory = memory
+        self.horizon = horizon
+        self.batch_size = batch_size
+        self.flatten_xs = flatten_xs
+        self.scaler = StandardScaler()
+        self.setup()
+
+    def _dl(self, array):
+        ds = TSDataset(torch.tensor(array).float(),
+                       memory=self.memory,
+                       horizon=self.horizon,
+                       flatten=self.flatten_xs)
+        dl = DataLoader(ds,
+                        batch_size = self.batch_size,
+                        shuffle = False, num_workers = 1)
+        return dl
+
+    def setup(self):
+        y_train_and_val, y_test = temporal_train_test_split(self.series, test_size=0.1)
+        y_train, y_val = temporal_train_test_split(y_train_and_val, test_size=0.1)
+
+        self.y_train, self.y_val, self.y_test = y_train, y_val, y_test
+        self.y_train = self.scaler.fit_transform(self.y_train.to_numpy().reshape(-1, 1)).squeeze()
+        self.y_val = self.scaler.transform(self.y_val.to_numpy().reshape(-1, 1)).squeeze()
+        self.y_test = self.scaler.transform(self.y_test.to_numpy().reshape(-1, 1)).squeeze()
+
+    def train_dataloader(self):
+        return self._dl(self.y_train)
+
+    def val_dataloader(self):
+        return self._dl(self.y_val)
+
+    def test_dataloader(self):
+        return self._dl(self.y_test)
+
 if __name__ == '__main__':
-    dataset = TSDataset(
-        np.array(list(range(0, 32))),
-        memory=8, horizon=4,
-        flatten=False
-    )
 
-    for x, y in dataset:
-        x_i, y_i = x, y
+    print(TSDataset.__name__)
+
+    df = pd.read_parquet("/Users/jonasb/repos/ModelarDB-ext/data/REDD-Cleaned-f32/house_1-channel_1_output_data_points.parquet")
+    dm = DataModule(df, 16, 4, 32, False, None)
+
+    for x, y in dm.train_dataloader():
+        x_id, y_id = x, y
         break
-
-    print(len(dataset))
--- forecasting/forecasting.py
+++ forecasting/forecasting.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-df = pd.read_parquet("/data/REDD-Cleaned-f32/house_1-channel_1_output_data_points.parquet")
+from dataset import *
+
+df = pd.read_parquet("/Users/jonasb/repos/ModelarDB-ext/data/REDD-Cleaned-f32/house_1-channel_1_output_data_points.parquet")
 DATETIME_COL = 'datetime'
 
 df[DATETIME_COL] = pd.to_datetime(df[DATETIME_COL], unit='ms')
@@ -15,4 +17,9 @@ from sktime.forecasting.model_selection import temporal_train_test_split
 y_df = df[f'value-E10']
 y_train, y_test = temporal_train_test_split(y_df, test_size=0.1)
 
+y_train_ds = TSDataset(y_train, 8, 4)
+y_train_dl = DataLoader(y_train_ds,
+                         batch_size = 128,
+                         shuffle = False,
+                         num_workers = 1)
 
