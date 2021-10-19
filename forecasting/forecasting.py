f20bfd725a0f90732226eeb2b688822e1b3a3ffb
Jonas Brusokas <jbrusokas@gmail.com>
2021-10-19 15:41:11 +0200

    Added turbo_lstm and running experiments on it

--- forecasting/forecasting.py
+++ forecasting/forecasting.py
@@ -9,7 +9,6 @@ from sklearn.metrics import mean_squared_error
 if __name__ == '__main__':
 
     def test_model(model, dl, scaler: StandardScaler, metric = root_mean_squared_error()):
-        # TODO: remember to compare against RAW dl also
         list_test_rmse = []
         list_ys = []
         for x, y in dl:
@@ -94,91 +93,105 @@ if __name__ == '__main__':
             ys_df.to_csv(ys_output_file_path)
 
         # print(f"Test RMSE: {test_rmse}")
-        return model, float(test_rmse), float(raw_test_rmse)
+        return model, float(epoch_loss), float(test_rmse), float(raw_test_rmse)
 
     horizon = 30
     memory = 60
-    batch_size = 1024 * 2
-    hidden_size = 32
+    batch_size = 512
+    hidden_size = 16
     epochs = 15
     learning_rate = 0.005
     lr_gamma = 0.9
 
     before_everything = DateUtils.now()
 
-    output_parent_folder = f"{os.path.join(FileUtils.project_root_dir(), 'results', 'forecasting_results', '1018', f'{before_everything.hour}-{before_everything.minute}')}"
+    output_super_parent_folder = f"{os.path.join(FileUtils.project_root_dir(), 'results', 'forecasting_results', f'{before_everything.month}{before_everything.day}')}"
+    output_parent_folder = f"{os.path.join(output_super_parent_folder, f'{before_everything.hour}-{before_everything.minute}')}"
     output_csv_path = f"{os.path.join(output_parent_folder, f'output_{before_everything.month}-{before_everything.day}_{before_everything.hour}-{before_everything.minute}.csv')}"
     FileUtils.create_dir(output_parent_folder)
 
     # TODO: fill dynamically
     parquet_file_paths = [
-        f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'lg_only','house_1-channel_1_output_data_points.parquet')}",
-        f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'lg_v3_d5', 'house_1-channel_1_output_data_points.parquet')}",
-        f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'lg_v3_d10', 'house_1-channel_1_output_data_points.parquet')}",
-        f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'lg_v3_d25', 'house_1-channel_1_output_data_points.parquet')}",
-        f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'pmc_only', 'house_1-channel_1_output_data_points.parquet')}",
+        # f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'lg_only','house_1-channel_1_output_data_points.parquet')}",
+        # f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'lg_v3_d5', 'house_1-channel_1_output_data_points.parquet')}",
+        # f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'lg_v3_d10', 'house_1-channel_1_output_data_points.parquet')}",
+        # f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'lg_v3_d25', 'house_1-channel_1_output_data_points.parquet')}",
+        # f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'pmc_only', 'house_1-channel_1_output_data_points.parquet')}",
+        f"{os.path.join(FileUtils.project_root_dir(), 'data', 'REDD-Cleaned-f32', 'swing', 'house_1-channel_1_output_data_points.parquet')}",
     ]
 
     error_bound_list = [0, 1, 2, 5, 10, 25, 50, None]
-    model_type_list = ['lstm', 'lr']
+    model_type_list = ['turbo_lstm']
 
-    total_run_count = len(error_bound_list) * len(model_type_list) * len(parquet_file_paths)
+    hidden_size_list = [hidden_size]
+    # hidden_size_list = [4, 8, 16, 32, 48, 80] # TODO: remove for LR
+
+    total_run_count = len(error_bound_list) * len(model_type_list) * len(parquet_file_paths) * len(hidden_size_list)
 
     current_run = 0
     for parquet_path in parquet_file_paths:
         df = pd.read_parquet(parquet_path)
         for error_bound in error_bound_list:
             for model_type in model_type_list:
-                current_run+=1
-                print(f"Current run: {current_run} / {total_run_count} | Date: {DateUtils.now()}")
-                if (model_type == 'lstm'):
-                    model = BasicLSTM_simple(hidden_size=hidden_size,
-                                             output_length=horizon)
-                elif(model_type == 'lr'):
-                    model = LinearRegression(memory, horizon)
-                else:
-                    raise ValueError(f"Model type: '{model_type}' is unsupported!")
+                for hidden_size in hidden_size_list:
+                    current_run+=1
+                    print(f"Current run: {current_run} / {total_run_count} | Date: {DateUtils.now()}")
+                    # if (current_run <= 21):
+                    #     print("Skipping...")
+                    #     continue
+                    if (model_type == 'lstm'):
+                        model = BasicLSTM_simple(hidden_size=hidden_size,
+                                                 output_length=horizon)
+                    elif(model_type == 'lr'):
+                        model = LinearRegression(memory, horizon)
+                    elif(model_type == 'turbo_lstm'):
+                        model = LSTM_with_skip(memory_length=memory,
+                                               hidden_size=hidden_size,
+                                               output_length=horizon)
+                    else:
+                        raise ValueError(f"Model type: '{model_type}' is unsupported!")
 
-                flatten_xs = True if model_type in ['lr'] else False
+                    flatten_xs = True if model_type in ['lr'] else False
 
-                dataset_name = str(Path(parquet_path).parent.name)
-                before_training = DateUtils.now()
-                trained_model, rmse, raw_rmse = train_model(
-                    df,
-                    model=model,
-                    memory=memory,
-                    batch_size=batch_size,
-                    error_bound=error_bound,
-                    flatten_xs=flatten_xs,
-                    output_parent_folder=output_parent_folder,
-                    output_name=f"{model_type}_{dataset_name}_E{error_bound if not None else 'RAW'}"
-                )
-                output_dict = {
-                    'dataset_name': dataset_name,
-                    'model_type': model_type,
-                    'error_bound': error_bound if (error_bound is not None) else -1,
-                    'epochs': epochs,
-                    'memory': memory,
-                    'horizon': horizon,
-                    'batch_size': batch_size,
-                    'hidden_size': hidden_size if (model_type != 'lr') else -1,
-                    'rmse': rmse,
-                    'rmse_on_raw': raw_rmse,
-                    'train_start': before_training,
-                    'lr': learning_rate,
-                    'lr_gamma': lr_gamma,
-                }
-                output_csv_df = None
-                try:
-                    output_csv_df = pd.read_csv(output_csv_path)
-                    current_output_df = pd.DataFrame(output_dict, index=[1])
-                    output_csv_df = pd.concat([output_csv_df, current_output_df])
-                    print("Concatenating")
-                except Exception as e:
-                    print("Output file does not exist yet!")
-                    output_csv_df = pd.DataFrame(output_dict, index=[1])
-                output_csv_df.to_csv(path_or_buf=output_csv_path, index=False, header=True)
-                print(f"Training and inference took: {DateUtils.now() - before_training}")
+                    dataset_name = str(Path(parquet_path).parent.name)
+                    before_training = DateUtils.now()
+                    trained_model, train_rmse, rmse, raw_rmse = train_model(
+                        df,
+                        model=model,
+                        memory=memory,
+                        batch_size=batch_size,
+                        error_bound=error_bound,
+                        flatten_xs=flatten_xs,
+                        output_parent_folder=output_parent_folder,
+                        output_name=f"{model_type}_{dataset_name}_E{error_bound if not None else 'RAW'}"
+                    )
+                    output_dict = {
+                        'dataset_name': dataset_name,
+                        'model_type': model_type,
+                        'error_bound': error_bound if (error_bound is not None) else -1,
+                        'epochs': epochs,
+                        'memory': memory,
+                        'horizon': horizon,
+                        'batch_size': batch_size,
+                        'hidden_size': hidden_size if (model_type != 'lr') else -1,
+                        'train_rmse': train_rmse,
+                        'rmse': rmse,
+                        'rmse_on_raw': raw_rmse,
+                        'train_start': before_training,
+                        'lr': learning_rate,
+                        'lr_gamma': lr_gamma,
+                    }
+                    output_csv_df = None
+                    try:
+                        output_csv_df = pd.read_csv(output_csv_path)
+                        current_output_df = pd.DataFrame(output_dict, index=[1])
+                        output_csv_df = pd.concat([output_csv_df, current_output_df])
+                        print("Concatenating")
+                    except Exception as e:
+                        print("Output file does not exist yet!")
+                        output_csv_df = pd.DataFrame(output_dict, index=[1])
+                    output_csv_df.to_csv(path_or_buf=output_csv_path, index=False, header=True)
+                    print(f"Training and inference took: {DateUtils.now() - before_training}")
 
 
 
--- forecasting/models.py
+++ forecasting/models.py
@@ -62,6 +62,43 @@ class BasicLSTM_simple(nn.Module):
         y_pred = self.linear(lstm_out[:, self.output_length])
         return y_pred
 
+class LSTM_with_skip(nn.Module):
+    def __init__(self,
+                 memory_length,
+                 hidden_size,
+                 output_length,
+                 num_layers = 1,
+                 dropout = 0,
+                 n_features = 1,
+                 ):
+        super(LSTM_with_skip, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.n_features = n_features
+        self.dropout = dropout
+        self.output_length = output_length
+        self.memory_length = memory_length
+
+        self.lstm = nn.LSTM(input_size=n_features,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            dropout=dropout,
+                            batch_first=True)
+        self.input_linear = nn.Linear(in_features=self.n_features * self.memory_length, out_features=self.output_length)
+        self.lstm_out_linear = nn.Linear(self.hidden_size, self.output_length)
+        self.final_linear = nn.Linear(self.output_length * 2, self.output_length)
+
+    def forward(self, x):
+
+        lstm_out, other = self.lstm(x)
+
+        y_pred_lstm = self.lstm_out_linear(lstm_out[:, self.output_length])
+        y_pred_input_linear = self.input_linear(x.reshape([-1, self.n_features * self.memory_length]))
+        y_pred_concat = torch.cat([y_pred_lstm, y_pred_input_linear], axis=1)
+
+        y_pred = self.final_linear(y_pred_concat)
+        return y_pred
+
 if __name__ == '__main__':
     sequence_array = np.array([1,2,3,4,5,6])
     sequence_tensor = torch.from_numpy(sequence_array).unsqueeze(1).unsqueeze(0).float()
